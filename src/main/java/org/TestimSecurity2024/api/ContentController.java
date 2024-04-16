2f6fc1a01e0ab3d05ded376b7e77c21c5c9aa63c
epsilxn <amd7tashit@gmail.com>
2024-04-17 01:28:18 +0300

    Пока что работает

---
+++ src/main/java/org/TestimSecurity2024/api/ContentController.java
@@ -0,0 +1,21 @@
+package org.TestimSecurity2024.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContentController {
+    @GetMapping("/home")
+    public String handleWelcome() {
+        return "home";
+    }
+    @GetMapping("/admin/home")
+    public String handleAdminHome() {
+        return "home_admin";
+    }
+    @GetMapping("/user/home")
+    public String handleUserHome() {
+        return "home_user";
+    }
+}
---
+++ src/main/java/org/TestimSecurity2024/api/models/MyUserRepository.java
@@ -0,0 +1,9 @@
+package org.TestimSecurity2024.api.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MyUserRepository extends JpaRepository<MyUser, Long> {
+    Optional<MyUser> findByUsername(String username);
+}
